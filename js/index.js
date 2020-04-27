36e33d375c46cb0083e188f6632c978c722a843f
fjj <2730809743@qq.com>
2020-04-28 00:26:58 +0800

    使用canvas绘制蛇和食物

--- js/index.js
+++ js/index.js
@@ -1,26 +1,37 @@
-const snake = document.getElementById("snake");
-let speed = 5;
-document.onkeydown = function(event){
-    const e = event || window.event || arguments.callee.caller.arguments[0];
-    if(e){
-        switch (e.keyCode) {
-            case 38:
-                snake.style.top = snake.offsetTop - speed + "px";
-                break;
-            case 39:
-                snake.style.left = snake.offsetLeft + speed + "px";
-                break;
-            case 40:
-                snake.style.top = snake.offsetTop + speed + "px";
-                break;
-            case 37:
-                snake.style.left = snake.offsetLeft - speed + "px";
-                break;
-            default:
-                break;
-        }
-    }
-    if (snake.offsetLeft<0 || snake.offsetLeft>580 || snake.offsetTop<0 || snake.offsetTop>580){
-        alert("游戏结束！");
+const canvas = document.getElementById("my-canvas");
+const ctx = canvas.getContext("2d");
+let snake = [
+    {x: 2, y: 0},
+    {x: 1, y: 0},
+    {x: 0, y: 0},
+]
+drawFood();
+drawSnake();
+
+// 绘制坐标系
+for(let i=1; i<20; i++){
+    ctx.moveTo(0, i * 30 + 0.5);
+    ctx.lineTo(600, i * 30 + 0.5);
+    ctx.moveTo(i * 30 + 0.5, 0);
+    ctx.lineTo(i * 30 + 0.5, 600);
+}
+ctx.strokeStyle = "#fff";
+ctx.stroke();
+
+// 绘制食物
+function drawFood(){
+    const x = Math.floor( Math.random() * 20 ) *30;
+    const y = Math.floor( Math.random() * 20 ) *30;
+    ctx.fillStyle = "#FFC209";
+    ctx.fillRect(x, y, 30, 30);
+}
+
+// 绘制蛇
+function drawSnake(){
+    ctx.fillStyle = "red";
+    ctx.fillRect(snake[0].x * 30, snake[0].y * 30, 30, 30);
+    ctx.fillStyle = "#33399F";
+    for(let i=1; i<snake.length; i++){
+        ctx.fillRect(snake[i].x * 30, snake[i].y * 30, 30, 30);
     }
 }
