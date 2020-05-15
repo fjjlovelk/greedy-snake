de1e4317f4d359ecdf5ca020975b3f08b48ccb78
fjj <2730809743@qq.com>
2020-05-15 13:40:10 +0800

    修改

--- js/index.js
+++ js/index.js
@@ -1,96 +1,215 @@
-const canvas = document.getElementById("my-canvas");
-const ctx = canvas.getContext("2d");
-// 蛇的默认位置
-let snake = [
-    {x: 2, y: 0},
-    {x: 1, y: 0},
-    {x: 0, y: 0}
-];
-// 蛇的默认方向
-let dir = { x: 1, y: 0 };
-// 食物的默认位置
-let foodPos = {
-    x: Math.floor( Math.random() * 20 ),
-    y: Math.floor( Math.random() * 20 )
-};
-// 判断食物有没有被吃掉
-let isEated = false;
+const box = document.getElementById("box");
+const score1 = document.getElementById("score1");
+const score2 = document.getElementById("score2");
+const overDiv = document.getElementById("over");
+const overTip = document.getElementById("over-tip");
+let timer;
+let isPause = false;
+let isStart = false;
 let isOver = false;
 
-setInterval(() =>{
-    if(isOver){
-        document.getElementById("over").style.opacity = 0.6;
-        return;
+// 食物的构造方法
+class DrawFood {
+    constructor() {
+        this.width = 3;
+        this.height = 3;
     }
-    ctx.clearRect(0, 0, 600, 600);
-    if(isEated){
-        foodPos = {
-            x: Math.floor( Math.random() * 20 ),
-            y: Math.floor( Math.random() * 20 )
-        };
-        isEated = false;
+    display(){
+        const f = document.createElement("div");
+        this.state = f;
+        f.style.width = this.width + "rem";
+        f.style.height = this.height + "rem";
+        f.style.backgroundColor = "red";
+        f.style.position = "absolute";
+        this.x = Math.floor(Math.random() * 20);
+        this.y = Math.floor(Math.random() * 20);
+        f.style.left = this.x * this.width + "rem";
+        f.style.top = this.y * this.height + "rem";
+        box.appendChild(f);
     }
-    drawFood();
-    drawSnake();
-    drawLine();
-}, 1000/3);
+}
+const food = new DrawFood();
 
+// 蛇的构造方法
+class DrawSnake {
+    constructor() {
+        this.width = 3;
+        this.height = 3;
+        this.direction = "right";
+        this.body = [
+            {x: 2, y: 0},
+            {x: 1, y: 0},
+            {x: 0, y: 0}
+        ]
+    }
+    display() {
+        for(let i=0; i<this.body.length; i++){
+            const s = document.createElement("div");
+            this.body[i].state = s;
+            s.style.width = this.width + "rem";
+            s.style.height = this.height + "rem";
+            if(i===0){
+                s.style.backgroundColor = "green";
+                s.style.zIndex = 10;
+            }else{
+                s.style.backgroundColor = "#93CAAB";
+            }
+            s.style.position = "absolute";
+            s.style.left = this.body[i].x * this.width + "rem";
+            s.style.top = this.body[i].y * this.height + "rem";
+            box.appendChild(s);
+        }
+    }
+    move() {
+        clearInterval(timer);
+        timer = setInterval(() => {
+            // 清除蛇的身体
+            for(let i=0; i<this.body.length; i++){
+                if(this.body[i].state){
+                    box.removeChild(this.body[i].state);
+                }
+            }
+            // 吃到食物不删除最后一节，相当于增长一节
+            if(this.body[0].x == food.x && this.body[0].y == food.y){
+                score1.innerText ++;
+                box.removeChild(food.state);
+                food.display();
+            }else{
+                this.body.pop();
+            }
+            // 添加新的蛇头
+            const newHead = {
+                x: this.body[0].x,
+                y: this.body[0].y
+            };
+            switch (this.direction) {
+                case "up":
+                    newHead.y -= 1;
+                    break;
+                case "right":
+                    newHead.x += 1;
+                    break;
+                case "down":
+                    newHead.y += 1;
+                    break;
+                case "left":
+                    newHead.x -= 1;
+                    break;
+            }
+            this.body.unshift(newHead);
+            this.display();
+            // 蛇头碰到边界游戏结束
+            if(newHead.x < 0 || newHead.x >= 20 || newHead.y < 0 || newHead.y >= 20){
+                overGame()
+            }
+            // 蛇头碰到身体游戏结束
+            for(let i=1; i<this.body.length; i++){
+                if(this.body[0].x === this.body[i].x && this.body[0].y === this.body[i].y){
+                    overGame();
+                }
+            }
+            
+        }, 1000/3);
+    }
+}
+const snake = new DrawSnake();
+
+// 开始游戏
+function startGame(){
+    if(isStart) {
+        return;
+    }else{
+        food.display();
+        snake.display();
+        snake.move();
+        isStart = true;
+    }
+}
+// 暂停游戏
+function pauseGame(){
+    if(isStart){
+        overDiv.style.display = "flex";
+        overTip.style.display = "none";
+        clearInterval(timer);
+        isPause = true;
+    }
+}
+// 重新开始
+function reStart(){
+    box.innerHTML = "";
+    isStart = false;
+    isPause = false;
+    clearInterval(timer);
+    overDiv.style.display = "none";
+    snake.direction = "right";
+    snake.body = [
+        {x: 2, y: 0},
+        {x: 1, y: 0},
+        {x: 0, y: 0}
+    ]
+    score1.innerText = 0;
+    startGame();
+    console.log(snake.body);
+}
+// 返回游戏
+function backGame(){
+    if(isOver){
+        window.location.reload();
+    }else{
+        overDiv.style.display = "none";
+        snake.move();
+        isPause = false;
+    }
+}
+// 游戏结束
+function overGame(){
+    clearInterval(timer);
+    isOver = true;
+    score2.innerText = score1.innerText;
+    overDiv.style.display = "flex";
+    overTip.style.display = "inline-block";
+}
+
+// 4个控制按键
+function toUp(){
+    if(snake.direction != "down"){
+        snake.direction = "up";
+    }
+}
+function toRight(){
+    if(snake.direction != "left"){
+        snake.direction = "right";
+    }
+}
+function toDown(){
+    if(snake.direction != "up"){
+        snake.direction = "down";
+    }
+}
+function toLeft(){
+    if(snake.direction != "right"){
+        snake.direction = "left";
+    }
+}
+
+
+// 监听键盘方向键
 document.addEventListener("keydown", (event)=>{
     const e = event || window.event || arguments.callee.caller.arguments[0];
-    if(e){
+    if(e && !isPause){
         switch (e.keyCode) {
             case 38:
-                dir = { x: 0, y: -1 };
+                toUp();
                 break;
             case 39:
-                dir = { x: 1, y: 0 };
+                toRight();
                 break;
             case 40:
-                dir = { x: 0, y: 1 };
+                toDown();
                 break;
             case 37:
-                dir = { x: -1, y: 0 };
+                toLeft();
                 break;
         }
     }
 })
-
-// 绘制坐标系
-function drawLine(){
-    for(let i=1; i<20; i++){
-        ctx.moveTo(0, i * 30 + 0.5);
-        ctx.lineTo(600, i * 30 + 0.5);
-        ctx.moveTo(i * 30 + 0.5, 0);
-        ctx.lineTo(i * 30 + 0.5, 600);
-    }
-    ctx.strokeStyle = "#fff";
-    ctx.stroke();
-}
-// 绘制食物
-function drawFood(){
-    ctx.fillStyle = "#FFC209";
-    ctx.fillRect(foodPos.x * 30, foodPos.y * 30, 30, 30);
-}
-// 绘制蛇
-function drawSnake(){
-    ctx.fillStyle = "red";
-    ctx.fillRect(snake[0].x * 30, snake[0].y * 30, 30, 30);
-    ctx.fillStyle = "#33399F";
-    for(let i=1; i<snake.length; i++){
-        ctx.fillRect(snake[i].x * 30, snake[i].y * 30, 30, 30);
-    }
-    if(JSON.stringify(snake[0]) == JSON.stringify(foodPos)){
-        isEated = true;
-    }else{
-        snake.pop();
-    }
-    const oldHead = snake[0];
-    const newHead = {
-        x: oldHead.x + dir.x,
-        y: oldHead.y + dir.y
-    }
-    snake.unshift(newHead);
-    if(newHead.x < 0 || newHead.x >= 20 || newHead.y < 0 || newHead.y  >= 20){
-        isOver = true;
-    }
-}
